177bd991d6844be8117ff594c9ad4e383c41f955
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-10-16 15:38:13 +0200

    finish task 2

--- task-1/task-1.py
+++ task-1/task-1.py
@@ -2,9 +2,27 @@ from colorama import Fore, Back
 from pathlib import Path
 
 def total_salary(path):
+
+    '''
+    Calculates the total and average salary of developers from a given file.
+
+    Parameters:
+    path (str or Path): The path to the text file containing salary data. Each line in the file should represent one developer, with values separated by commas, in the format: 'name,salary'.
+
+    Returns:
+    tuple: A tuple containing two integers - the total salary and the average salary of all developers in the file.
+
+    Raises:
+    ValueError: If there is a formatting error in a line (e.g., missing or incorrect salary value).
+    FileNotFoundError: If the specified file is not found.
+    IOError: If there is an error reading the file.
+
+    Note:
+    The function prints a detailed error message in case of data format errors or file reading issues. In case of an error, it returns None.
+    '''
     
     try:
-        with open(path, 'r') as file:
+        with open(path, 'r', encoding='utf-8') as file:
             lines = file.readlines()
             
         total = 0
@@ -14,19 +32,19 @@ def total_salary(path):
                 _, salary = line.strip().split(',')
                 total += int(salary)
             except ValueError:
-                print(Fore.BLACK + Back.MAGENTA + f"Помилка в рядку: {line.strip()} - неправильний формат даних." + Fore.RESET + Back.RESET)
+                print(Back.RED + f"Помилка в рядку: {line.strip()} - неправильний формат даних." + Back.RESET)
                 return None
 
         average = int(total / len(lines))
         return (total, average)
     
     except FileNotFoundError:
-        print(Back.MAGENTA + f"Файл {path} не знайдено." + Back.RESET)
+        print(Back.RED + f"Файл {path} не знайдено." + Back.RESET)
         return None
     except IOError:
-        print(Back.MAGENTA + f"Сталася помилка при спробі читання файлу {path}." + Back.RESET)
+        print(Back.RED + f"Сталася помилка при спробі читання файлу {path}." + Back.RESET)
         return None
 
-path = Path('task-1.txt')            
+path = Path('salaries.txt')            
 total, average = total_salary(path)
-print(Back.YELLOW + f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}" + Back.RESET)
+print(Fore.BLACK + Back.YELLOW + f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}" + Back.RESET)
---
+++ task-2/task-2.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from colorama import Fore, Back
+
+def get_cats_info(path):
+    
+    '''
+    Retrieves information about cats from a specified file.
+
+    Parameters:
+    path (str or Path): The path to the text file containing cat data. Each line in the file should represent one cat, with values separated by commas in the format: 'id,name,age'.
+
+    Returns:
+    tuple: A tuple of dictionaries, where each dictionary contains the 'id', 'name', and 'age' of a cat.
+
+    Raises:
+    ValueError: If there is a formatting error in a line (e.g., missing or incorrect data).
+    FileNotFoundError: If the specified file is not found.
+    IOError: If there is an error reading the file.
+
+    Note:
+    The function prints a detailed error message in case of data format errors or file reading issues. In case of an error, it returns None.
+    '''
+
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        
+        cats_info = tuple()
+        
+        for line in lines:
+            try:
+                cat_info = dict()
+                id, name, age = line.strip().split(',')
+                cat_info['id'] = id
+                cat_info['name'] = name
+                cat_info['age'] = age
+                cats_info += (cat_info,)
+            except ValueError:
+                print(Back.RED + f"Error in line: {line.strip()} - incorrect data format." + Back.RESET)
+                return None
+        
+        return cats_info
+    
+    except FileNotFoundError:
+        print(Back.RED + f"The file {path} was not found." + Back.RESET)
+        return None
+    except IOError:
+        print(Back.RED + f"An error occurred while trying to read the file {path}." + Back.RESET)
+        return None
+
+
+
+path = Path('cats.txt')
+cats_info = get_cats_info(path)
+print(Fore.BLACK + Back.YELLOW + f'{cats_info}' + Fore.RESET + Back.RESET)
